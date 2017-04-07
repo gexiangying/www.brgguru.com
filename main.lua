3cdc37053b528edc30f805cf7a906fb4a9bd4f22
gexiangying <19727709@qq.com>
2017-04-07 15:20:18 +0800

    add index.html change usetlist port=8080

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 --liaoning ICP 16001504 password = "YFK787"
-hub_start("localhost",80,10,60,true,false,"hub.lua") --ip port max_accept max_accept_seconds,hubmode,accept and recv
+hub_start("localhost",8080,10,60,true,false,"hub.lua") --ip port max_accept max_accept_seconds,hubmode,accept and recv
 
 function socket_quit(content)
 	ip,port = hub_addr(content)
