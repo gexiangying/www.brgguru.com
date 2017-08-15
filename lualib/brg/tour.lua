ee36b2d4778de8884756d93daa2c24a9f9a62e73
gexiangying <19727709@qq.com>
2017-08-15 19:03:55 +0800

    edit jquery

--- lualib/brg/tour.lua
+++ lualib/brg/tour.lua
@@ -39,9 +39,9 @@ _ENV = M
 head = [[
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
 <meta name="viewport" content="width=device-width, initial-scale=1" > 
-<link rel="stylesheet" href="http://jquery.brgguru.com/jquery.mobile-1.4.5.min.css" />
-<script src="http://jquery.brgguru.com/jquery-1.11.1.min.js"></script>
-<script src="http://jquery.brgguru.com/jquery.mobile-1.4.5.min.js"></script>
+<link rel="stylesheet" href="jquery/jquery.mobile-1.4.5.min.css" />
+<script src="jquery/jquery-1.11.1.min.js"></script>
+<script src="jquery/jquery.mobile-1.4.5.min.js"></script>
 ]]
 --[[
 <div data-role="navbar">
